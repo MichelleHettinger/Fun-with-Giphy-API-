c856e24db8b3d6e58999d908b6344db20f62459a
Michelle Hettinger <Michelle Hettinger>
2016-07-23 14:23:40 -0400

    added toggle playing gif animation. modified css of gifs to appear nicely on page

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,41 +1,86 @@
  //Assign HTML elements to variables
  var animalButtonsList = $("#animalButtons");
- var animalForm = $("#animal-form");
+ var animalInput = $("#animal-input");
  var addAnimalButton = $("#addAnimal");
- var animalsDiv = $("#animals");
-
-//Set up the necessary query URL and empty array for pushing search terms
-var partialQueryURL = "http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=";
+ var gifDiv = $("#animals");
 
 
-//When document is ready, make add animal button clickable
- $(document).ready(function(){
+ 	//When animal button is clicked...
  	addAnimalButton.on("click", function(){
 
+ 		//Animal becomes the string that was typed in
+    	var animal = animalInput.val();		
+    	console.log(animal);
+
+    	//Generate a new button with the attribute "data-animal", bootstrap classes and text containing the search terms
+    	var newButton = $("<button>");
+    	newButton.attr("data-animal", animal);
+    	newButton.addClass("btn btn-primary");
+    	newButton.html(animal);
+
+    	animalButtonsList.append(newButton);
 
 
+	 	//When any button tag is clicked...
+	 	$("button").on("click", function(){
+	 		console.log("it works");
+
+	 		//The search terms are set to the data-animal value set in the addAnimalButton button
+			var animalSearch = $(this).data('animal');
+	 		var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + animalSearch + "&api_key=dc6zaTOxFJmzC&limit=12";
+
+
+			$.ajax({url: queryURL, method: 'GET'}).done(function(response){
+				
+				console.log(queryURL);
+				console.log(response);
+
+				var results = response.data;
+
+				for (i=0; i<results.length; i++){
+
+					var animalDiv = $("<div>");
+
+					//Make a paragraph tag and populate it with rating
+				    var p = $("<p>");
+				   	p.text("Rating: " + results[i].rating);
+
+				   	//Make an image tag and give it src, data-state, data-still and data-animate attributes
+					var imageTag = $("<img>");
+					imageTag.attr("src", results[i].images.fixed_height_still.url);
+					imageTag.attr("data-still", results[i].images.fixed_height_still.url);
+					imageTag.attr("data-animate", results[i].images.fixed_height.url);
+
+
+					//Append both rating and the gif to the page
+					animalDiv.append(p);
+					animalDiv.append(imageTag);
+					animalDiv.addClass("adjustGif");
+
+					gifDiv.prepend(animalDiv);
+
+				}
+
+				//Toggle for playing and stopping the gif animation
+				$("img").on("click", function(){
+					var state = $(this).attr("data-state");
+
+					if (state == 'still'){
+						$(this).attr('src', $(this).data('animate'));
+						$(this).attr('data-state', 'animate');
+					}
+					else{
+						$(this).attr('src', $(this).data('still'));
+						$(this).attr('data-state', 'still');
+					}
+				})
+			})     
+	 	})
  	})
-})
 
 
 
-var queryURL = partialQueryURL + searchTag;
-
-
-$.ajax({url: queryURL, method: 'GET'}).done(function(response){
-	
-	console.log(queryURL);
-	console.log(response);
-	console.log(response.Runtime);
 
 
 
-	 var imageTag = $("<img>");
-	 imageTag.attr("src", imageURL)
-
-
-	 animalsDiv.append(imageTag)
-
-
-})     
 
