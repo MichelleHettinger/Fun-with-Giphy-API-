0c1616ae2e6d62370c22dbf9acb1bafe90cad918
Michelle Hettinger <Michelle Hettinger>
2016-07-23 11:52:47 -0400

    assigned html elements to js variables. established partialQueryURL containing the giphy api url. made onclick event for add animal button. set up ajax call to get a giph url and append to the animalsDiv

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,41 @@
- 
+ //Assign HTML elements to variables
+ var animalButtonsList = $("#animalButtons");
+ var animalForm = $("#animal-form");
+ var addAnimalButton = $("#addAnimal");
+ var animalsDiv = $("#animals");
+
+//Set up the necessary query URL and empty array for pushing search terms
+var partialQueryURL = "http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=";
+
+
+//When document is ready, make add animal button clickable
+ $(document).ready(function(){
+ 	addAnimalButton.on("click", function(){
+
+
+
+ 	})
+})
+
+
+
+var queryURL = partialQueryURL + searchTag;
+
+
+$.ajax({url: queryURL, method: 'GET'}).done(function(response){
+	
+	console.log(queryURL);
+	console.log(response);
+	console.log(response.Runtime);
+
+
+
+	 var imageTag = $("<img>");
+	 imageTag.attr("src", imageURL)
+
+
+	 animalsDiv.append(imageTag)
+
+
+})     
+
